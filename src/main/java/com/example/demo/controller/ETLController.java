f0e193394243d83b8300ee94b8e541b6efc09b33
catherinebalajadia <catb_23@hotmail.com>
2024-05-30 12:22:48 -0600

    remove unused import

--- src/main/java/com/example/demo/controller/ETLController.java
+++ src/main/java/com/example/demo/controller/ETLController.java
@@ -4,12 +4,11 @@ import com.example.demo.dto.WagerSummary;
 import com.example.demo.service.ETLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
