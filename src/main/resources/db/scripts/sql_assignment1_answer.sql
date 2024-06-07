9ba1145a5ce15e7b129c167afaa05db0e801f235
catherinebalajadia <catb_23@hotmail.com>
2024-06-07 10:16:35 -0600

    update to simpler query

--- src/main/resources/db/scripts/sql_assignment1_answer.sql
+++ src/main/resources/db/scripts/sql_assignment1_answer.sql
@@ -1,32 +1,39 @@
-WITH CTE_SETTLEMENT_PROPORTION AS
-    (
-        SELECT
-            Stake.BET_ID,
-            Stake.ACCOUNT_ID,
-            Stake.TRANSACTION_TYPE,
-            Stake.AMOUNT,
-            Stake.FUND_TYPE,
-            Settlement.AMOUNT SettlementAmount,
-            Stake.Amount / Settlement.AMOUNT Ratio,
-
-            Settlement.Amount / SUM(Stake.Amount) OVER(PARTITION BY Stake.BET_ID, Stake.ACCOUNT_ID) Multiplier
-
-    FROM
-   (
-    SELECT BET_ID, ACCOUNT_ID, TRANSACTION_TYPE, AMOUNT, FUND_TYPE FROM StakeSettlement WHERE TRANSACTION_TYPE = 'STAKE'
-    ) Stake
-    LEFT JOIN
-   (
-    SELECT BET_ID, AMOUNT FROM StakeSettlement WHERE TRANSACTION_TYPE = 'SETTLEMENT'
-    ) Settlement
-ON Stake.BET_ID = Settlement.BET_ID
-
-)
-
+WITH Stake AS (
+    SELECT BET_ID, ACCOUNT_ID, TRANSACTION_TYPE, AMOUNT, FUND_TYPE
+    FROM StakeSettlement
+    WHERE TRANSACTION_TYPE = 'STAKE'
+),
+     Settlement AS (
+         SELECT BET_ID, AMOUNT
+         FROM StakeSettlement
+         WHERE TRANSACTION_TYPE = 'SETTLEMENT'
+     ),
+     TotalStakes AS (
+         SELECT BET_ID, ACCOUNT_ID, SUM(AMOUNT) AS TotalAmount
+         FROM Stake
+         GROUP BY BET_ID, ACCOUNT_ID
+     ),
+     CTE_SETTLEMENT_PROPORTION AS (
+         SELECT
+             Stake.BET_ID,
+             Stake.ACCOUNT_ID,
+             Stake.TRANSACTION_TYPE,
+             Stake.AMOUNT,
+             Stake.FUND_TYPE,
+             Settlement.AMOUNT AS SettlementAmount,
+             Stake.Amount / Settlement.AMOUNT AS Ratio,
+             Settlement.Amount / TotalStakes.TotalAmount AS Multiplier
+         FROM
+             Stake
+                 LEFT JOIN
+             Settlement ON Stake.BET_ID = Settlement.BET_ID
+                 LEFT JOIN
+             TotalStakes ON Stake.BET_ID = TotalStakes.BET_ID AND Stake.ACCOUNT_ID = TotalStakes.ACCOUNT_ID
+     )
 SELECT
     BET_ID,
     ACCOUNT_ID,
-    SUM(CASE WHEN FUND_TYPE = 'CASH' THEN (SettlementAmount * Ratio * Multiplier) ELSE 0 END) CASH_SETTLEMENT,
-    SUM(CASE WHEN FUND_TYPE = 'BONUS' THEN (SettlementAmount * Ratio * Multiplier) ELSE 0 END) BONUS_SETTLEMENT
+    SUM(CASE WHEN FUND_TYPE = 'CASH' THEN (SettlementAmount * Ratio * Multiplier) ELSE 0 END) AS CASH_SETTLEMENT,
+    SUM(CASE WHEN FUND_TYPE = 'BONUS' THEN (SettlementAmount * Ratio * Multiplier) ELSE 0 END) AS BONUS_SETTLEMENT
 FROM CTE_SETTLEMENT_PROPORTION
-GROUP BY BET_ID, ACCOUNT_ID
+GROUP BY BET_ID, ACCOUNT_ID;
